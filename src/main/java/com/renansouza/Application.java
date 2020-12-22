eba8b06c0c73fc5f16cc5cfb4c62c2c6ba7fdeac
Renan Souza <renansouzprojuris.com.br>
2020-12-21 23:23:25 -0300

    Minor fixes and bump version.

--- src/main/java/com/renansouza/Application.java
+++ src/main/java/com/renansouza/Application.java
@@ -7,9 +7,8 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.annotations.tags.Tag;
+
 
 @OpenAPIDefinition(
         info = @Info(
